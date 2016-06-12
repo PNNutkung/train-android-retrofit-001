e3cc8872208f3522039a675a8f5247085e4e9730
PNNutkung <pipatpol.t@ku.th>
2016-06-12 22:03:00 +0700

    Add 3 fragments

---
+++ app/src/main/java/com/example/nut/sampleretrofit/Fragments/LoadingFragment.java
@@ -0,0 +1,39 @@
+package com.example.nut.sampleretrofit.Fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.nut.sampleretrofit.R;
+
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+
+public class LoadingFragment extends Fragment {
+
+    private Unbinder unbinder;
+
+    public static LoadingFragment newInstance() {
+        LoadingFragment fragment = new LoadingFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_loading, container, false);
+        unbinder = ButterKnife.bind(this, rootView);
+        return rootView;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+}
